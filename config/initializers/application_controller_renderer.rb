1c8f87205ba9f2ee4adb959d61b8c894922177e3
MOEKASAKAI <wpi.floves2@gmail.com>
2021-01-22 07:31:48 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2b1e1a63027c36d5a2fb69048c34de7a92ffe76a6097e7c4bc7ab1405afbbcc8095183df1abca52626db2c2751589eda9a27133771c32bba62c1eff21f129d68'
