d2667a301a6ce696ca2047d6d47d30047a1e906e
MOEKASAKAI <wpi.floves2@gmail.com>
2021-01-22 06:13:10 +0000

    [Finish] 2-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,19 +3,29 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
+  # 以下を追加
   def create
-  # １. データを新規登録するためのインスタンス作成
-  list = List.new(list_params)
-  # ２. データをデータベースに保存するためのsaveメソッド実行
-  list.save
-  # ３. トップ画面へリダイレクト
-  redirect_to '/top'
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # ３. redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+  
+  def index
+    @lists = List.all
+  end
+  
+  def show
+    @list = List.find(params[:id])
   end
 
   private
-# ストロングパラメータ
+  # ストロングパラメータ
   def list_params
-  params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body)
   end
-
+  
 end
