c3c5464ed8e318947de0eacc9ff4b7c03a74952f
MOEKASAKAI <wpi.floves2@gmail.com>
2021-01-22 07:01:22 +0000

    [Finish] 2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -13,13 +13,21 @@ class TodolistsController < ApplicationController
     # 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
   private
@@ -27,5 +35,5 @@ class TodolistsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
+
 end
